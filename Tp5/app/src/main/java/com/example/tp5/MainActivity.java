324b192dcdccbfb4e4583dd42ed16e51d7909670
Ahmetkan <ahmetkan-18@hotmail.fr>
2021-03-31 15:57:52 +0200

    tp5

--- Tp5/app/src/main/java/com/example/tp5/MainActivity.java
+++ Tp5/app/src/main/java/com/example/tp5/MainActivity.java
@@ -1,14 +1,44 @@
 package com.example.tp5;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    private RecyclerView mRecyclerView;
+    private MonRecyclerViewAdapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mAdapter = new MonRecyclerViewAdapter(getDataSource());
+        mRecyclerView.setAdapter(mAdapter);
+    }
+    private ArrayList<Donnee> getDataSource() {
+        ArrayList results = new ArrayList<Donnee>();
+        for (int index = 0; index < 20; index++) {
+            Donnee obj = new Donnee("Texte principal " + index,"Texte auxiliaire " + index);
+            results.add(index, obj);
+        }
+        return results;
+    }
+    public interface DetecteurDeClicSurRecycler {
+        public void clicSurRecyclerItem(int position, View v);
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mAdapter.setDetecteurDeClicSurRecycler(this);
     }
 }
